aa669c32aa652e0c72c45949658df37602f61033
hyunji <phj9573@naver.com>
2024-06-06 16:13:00 +0900

    인터페이스 구축

---
+++ BaseballTimes/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  BaseballTimes
+//
+//  Created by ParkHyunji on 6/6/24.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
